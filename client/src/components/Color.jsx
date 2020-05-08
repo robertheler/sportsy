d95d314f58f43f4b52c0e3bbeb8097f6a7e3853f
Robert Heler <robertheler@gmail.com>
2020-05-08 11:11:52 -0700

    Added hovering opacity and border effects to Color components

--- client/src/components/Color.jsx
+++ client/src/components/Color.jsx
@@ -13,6 +13,13 @@ const StyledImage = styled.div`
   background: url(${props => props.url});
   background-size: 100%;
   margin: 0 5px;
+  opacity: ${props => props.isSelected === 'true' ? "1" : "0.5"};
+  box-sizing: border-box;
+  border-bottom: ${props => props.isSelected === 'true' ? "3px solid black" : "none"};
+  box-sizing: border-box;
+  &:hover {
+    opacity: 1;
+  }
 `;
 
 //TODO
@@ -33,7 +40,7 @@ class Color extends Component{
     let url = fetchImageLocation(this.props.product.colors[this.props.color].images[0]);
     return (
       <RenderDiv>
-        <StyledImage url={url} onClick={this.handleColorChange}/>
+        <StyledImage url={url} onClick={this.handleColorChange} isSelected={this.props.isSelected.toString()}/>
       </RenderDiv>
     );
   }
